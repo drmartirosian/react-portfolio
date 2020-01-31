86b7056b687c5601d8eaecd9b5368c641d537435
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-01-31 13:47:01 -0600

    Resume page done

--- src/pages/ResumePage/ResumePage.jsx
+++ src/pages/ResumePage/ResumePage.jsx
@@ -6,12 +6,13 @@ class ResumePage extends Component {
         super();
         this.state = { user: userService.getUser() };
     }
-
-
     render() {
         return (
         <div>
             <h1>RESUME</h1>
+            <a href="https://docs.google.com/document/d/1F1marCRFsup2EU_Wz96aagykpXIXkox8NvWNTqJfIcc/edit?usp=sharing" target="_blank">
+                <img src="https://i.imgur.com/QB0ruWm.png" alt="Resume"></img>
+            </a>
         </div>
         );
     }
